1d4760bd34f2859d4541ffddde39952bed439d79
DESKTOP-6LPJTTU\olik3 <olik369@gmail.com>
2021-04-12 19:31:50 +0900

    2021-04-12

---
+++ 7장/Chp01.BasicClass/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace Chp01.BasicClass
+{
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            WriteLine($"{Name} : 야옹");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.Color = "하얀색";
+            kitty.Name = "키티";
+            kitty.Meow();
+            WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat();
+            nero.Color = "검은색";
+            nero.Name = "네로";
+            nero.Meow();
+            WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+}
---
+++ 7장/chp03.Global/MainApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace chp03.Global
+{
+    class Global
+    {
+        public static int Count = 0;
+    }
+
+    class ClassA
+    {
+        public ClassA()
+        {
+            Global.Count++;
+        }
+    }
+
+    class ClassB
+    {
+        public ClassB()
+        {
+            Global.Count++;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"Global.Count : {Global.Count}");
+
+            new ClassA();
+            new ClassA();
+            new ClassB();
+            new ClassB();
+
+            WriteLine($"Global.Count : {Global.Count}");
+        }
+    }
+}
